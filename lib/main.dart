468978b250a3dba04d70e8a90cf9bde7f493afcb
pllee4 <pinloon_0428@hotmail.com>
2021-05-23 17:09:47 +0800

    edited some styling for markdown

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return DynamicTheme(
-      defaultBrightness: Brightness.light,
+      defaultBrightness: Brightness.dark,
       data: (brightness) {
         return brightness == Brightness.light
             ? ThemeData(
@@ -50,15 +50,25 @@ class _MyAppState extends State<MyApp> {
                 unselectedWidgetColor: Colors.blueGrey[300],
                 cardColor: Colors.blueGrey[50],
                 primaryTextTheme: TextTheme(
+                  headline5:
+                      TextStyle(color: Colors.blueGrey[300], fontSize: 20),
+                  headline6:
+                      TextStyle(color: Colors.blueGrey[300], fontSize: 18),
+                  bodyText1:
+                      TextStyle(color: Colors.blueGrey[300], fontSize: 18),
+                  bodyText2:
+                      TextStyle(color: Colors.blueGrey[300], fontSize: 18),
                   button: TextStyle(
                     color: Colors.blueGrey,
                     decorationColor: Colors.blueGrey[300],
                   ),
-                  subtitle2: TextStyle(
-                    color: Colors.blueGrey[900],
-                  ),
                   subtitle1: TextStyle(
                     color: Colors.black,
+                    fontSize: 18,
+                  ),
+                  subtitle2: TextStyle(
+                    color: Colors.blueGrey[900],
+                    fontSize: 18,
                   ),
                   headline1: TextStyle(color: Colors.blueGrey[800]),
                 ),
@@ -73,19 +83,27 @@ class _MyAppState extends State<MyApp> {
                 accentColor: Colors.white,
                 unselectedWidgetColor: Colors.blueGrey[300],
                 cardColor: Colors.black,
+                dividerColor: Colors.blueGrey[900].withOpacity(0.6),
                 primaryTextTheme: TextTheme(
+                  headline5: TextStyle(color: Colors.white, fontSize: 20),
+                  headline6: TextStyle(color: Colors.white70, fontSize: 18),
+                  bodyText1: TextStyle(color: Colors.white, fontSize: 18),
+                  bodyText2: TextStyle(color: Colors.white, fontSize: 18),
                   button: TextStyle(
                     color: Colors.blueGrey[200],
                     decorationColor: Colors.blueGrey[50],
                   ),
-                  subtitle2: TextStyle(
-                    color: Colors.white,
-                  ),
                   subtitle1: TextStyle(
                     color: Colors.blueGrey[300],
+                    fontSize: 18,
+                  ),
+                  subtitle2: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
                   headline1: TextStyle(
                     color: Colors.white70,
+                    fontSize: 18,
                   ),
                 ),
                 pageTransitionsTheme: NoTransitionsOnWeb(),
