7f455397ac40fe1a0dd02eab15369cb4b01571a5
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-02 21:33:39 +0800

    lib: main.dart: change routing to have 0 transition duration

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,56 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+Route<dynamic> _generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case '/Home':
+      return CustomPageRoute(
+          routeName: "/Home",
+          builder: (context) => CustomPage(pageName: "Home"));
+    case '/About':
+      return CustomPageRoute(
+          routeName: "/About",
+          builder: (context) => CustomPage(pageName: "About"));
+    case '/Technical Notes':
+      return CustomPageRoute(
+          routeName: "/Technical Notes",
+          builder: (context) => CustomPage(pageName: "Technical Notes"));
+    case '/Projects':
+      return CustomPageRoute(
+          routeName: "/Projects",
+          builder: (context) => CustomPage(pageName: "Projects"));
+    case '/Achievements':
+      return CustomPageRoute(
+          routeName: "/Achievements",
+          builder: (context) => CustomPage(pageName: "Achievements"));
+    default:
+      return CustomPageRoute(
+          routeName: "/Home",
+          builder: (context) => CustomPage(pageName: "Home"));
+  }
+}
+
+class CustomPageRoute extends MaterialPageRoute {
+  final String routeName;
+  CustomPageRoute({Key key, @required this.routeName, builder})
+      : super(builder: builder, settings: RouteSettings(name: routeName));
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 0);
+}
+
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Pin Loon Lee',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.from(colorScheme: const ColorScheme.light()),
-        darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
-        initialRoute: '/Home',
-        routes: {
-          '/Home': (context) => CustomPage(pageName: "Home"),
-          '/About': (context) => CustomPage(pageName: "About"),
-          '/Technical Notes': (context) =>
-              CustomPage(pageName: "Technical Notes"),
-          '/Projects': (context) => CustomPage(pageName: "Projects"),
-          '/Achievements': (context) => CustomPage(pageName: "Achievements"),
-        });
+      title: 'Pin Loon Lee',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.from(colorScheme: const ColorScheme.light()),
+      darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
+      onGenerateRoute: _generateRoute,
+      initialRoute: '/Home',
+      onGenerateInitialRoutes: (initialRoute) =>
+          [_generateRoute(RouteSettings(name: initialRoute))],
+    );
   }
 }
