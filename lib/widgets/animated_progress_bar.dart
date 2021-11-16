38471676426fc5cb6b1f1d3605bf5e62f32fb681
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-16 23:20:27 +0800

    lib: widgets: added animated_progress_bar

---
+++ lib/widgets/animated_progress_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class AnimatedProgressBar extends StatefulWidget {
+  final double percentage;
+  final String label;
+  AnimatedProgressBar(
+      {Key key, @required this.percentage, @required this.label})
+      : super(key: key);
+
+  @override
+  _AnimatedProgressBarState createState() => _AnimatedProgressBarState();
+}
+
+class _AnimatedProgressBarState extends State<AnimatedProgressBar>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _animation;
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animation =
+        Tween(begin: 0.0, end: widget.percentage).animate(_animationController)
+          ..addListener(() {
+            setState(() {});
+          });
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(AnimatedProgressBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _animationController.duration = Duration(seconds: 1);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Text(
+        widget.label,
+        style: TextStyle(color: Colors.white),
+      ),
+      SizedBox(height: 30),
+      LinearProgressIndicator(
+        value: (_animation.value) / 100.0,
+        color: Colors.teal[800],
+        backgroundColor: Colors.black,
+      ),
+      SizedBox(height: 30),
+      Text((_animation.value).toInt().toString() + "%"),
+    ]);
+  }
+}
