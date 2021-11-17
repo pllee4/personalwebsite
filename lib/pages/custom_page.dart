5e267df76357e500a95faf26b79cf0c8b3cc119e
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-17 22:26:23 +0800

    lib: pages: moved home page into home.dart

--- lib/pages/custom_page.dart
+++ lib/pages/custom_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:personalwebsite/pages/about.dart';
+import 'package:personalwebsite/pages/home.dart';
 // import 'package:personalwebsite/pages/technical_notes.dart';
 import 'package:personalwebsite/widgets/nav_bar.dart';
-import 'package:personalwebsite/widgets/quote_slider.dart';
 import 'package:personalwebsite/widgets/responsive_widget.dart';
 
 class CustomPage extends StatelessWidget {
@@ -48,31 +48,10 @@ class CustomPage extends StatelessWidget {
         ])));
   }
 
-  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
-    onPrimary: Colors.white,
-    primary: Colors.teal[900],
-    minimumSize: Size(140, 70),
-    padding: EdgeInsets.symmetric(horizontal: 16),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(4)),
-    ),
-  );
-
   List<Widget> getCustomPage(String pageName, BuildContext context) {
     switch (pageName) {
       case "Home":
-        return [
-          SizedBox(height: (MediaQuery.of(context).size.height) / 3.5),
-          QuoteSlider(),
-          ElevatedButton(
-            style: raisedButtonStyle,
-            onPressed: () {
-              Navigator.pushNamed(context, '/About');
-            },
-            child: Text('EXPLORE NOW',
-                style: TextStyle(fontFamily: 'Montserrat', fontSize: 30)),
-          )
-        ];
+        return [Home()];
       case "About":
         return [About()];
       case "Technical Notes":
